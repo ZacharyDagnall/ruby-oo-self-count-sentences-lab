3644c5b92b56dd2e7a789196fe11fbaf623b9abf
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-12 13:57:13 -0500

    commit tree 9f402e0743709402a449e0d4b7d78810cecb272a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,23 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    #because i'm a linguistics geek i'd like to point out that i think what is meant here is "declarative or imperative sentence". Questions are interrogative sentences, but sentences nonetheless.
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr = self.split(/[.?!]/) #any of the three delimiters will split the string
+    arr.delete("")            #removes any of these which may have been created
+                              #by back-to-back delimiters
+    arr.length
   end
 end
